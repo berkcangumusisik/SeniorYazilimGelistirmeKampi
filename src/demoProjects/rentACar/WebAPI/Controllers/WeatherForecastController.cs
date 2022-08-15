67514bf3d0018df288cf64a8340ac2483b20aa1e
berkcangumusisik <gumusisikberkcan@gmail.com>
2022-08-15 23:59:11 +0300

    Lesson 1 updated.

---
+++ src/demoProjects/rentACar/WebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+    }
+}
